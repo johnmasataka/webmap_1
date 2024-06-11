73520e0678339bede969c24bdf6a90eae8829ec2
johnmasataka <johnmasataka@outlook.com>
2024-06-11 22:58:20 +0900

    initial import

--- map.js
+++ map.js
@@ -1,12 +1,1 @@
-'use strict';
-
-console.log('Loaded map.js');
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obm1hc2F0YWthIiwiYSI6ImNseDl0ZnByMzJ1cnQyam9lbnB5ajQ3d3EifQ.vDNx6tuvH4CxRBRu-Tt0Tg';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: [-73.93324, 40.80877],
-    zoom: 14
-});
+console.log("Hello, world!")
